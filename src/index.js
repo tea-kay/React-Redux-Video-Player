5b7c53a366bc8c93a1c4f235b5385e959fe296cd
Timothy Kim <tkim1202@gmail.com>
2017-12-07 18:46:48 +0900

    Passed YT api data prop to video_list component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-const App = () => {
-  return (
-  <div>
-    <SearchBar />
-  </div>
-  );
+const API_KEY = process.env.API_KEY;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'cryptocurrencies'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={ this.state.videos }/>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
