2ca4b3f995f6bbefb88d915e40643370d83cd9ad
Timothy Kim <tkim1202@gmail.com>
2017-12-08 17:21:47 +0900

    Added lodash and search throttle

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -16,18 +17,23 @@ class App extends Component {
       videos: [],
       selectedVideo: null
      };
+     this.videoSearch('cryptocurrencies');
+  }
 
-    YTSearch({ key: API_KEY, term: 'cryptocurrencies'}, (videos) => {
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
       });
     });
   }
+
   render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 300)
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={ this.state.selectedVideo } />
         <VideoList
           onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
