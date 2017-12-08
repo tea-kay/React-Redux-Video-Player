9d55e2dac6166aa7b490ad405203dc8f47edeaf5
Timothy Kim <tkim1202@gmail.com>
2017-12-08 13:46:23 +0900

    Added key file to protect API key

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import { keys } from '../.keys.js';
 
-const API_KEY = process.env.API_KEY;
+const API_KEY = keys.YT_KEY;
 
 class App extends Component {
   constructor(props) {
