ef3cca0d9d1b1370345872910de3afa63d9a28a7
Timothy Kim <tkim1202@gmail.com>
2017-12-08 16:23:15 +0900

    Added onselect state to video items

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import { keys } from '../.keys.js';
 
 const API_KEY = keys.YT_KEY;
@@ -11,17 +12,26 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+     };
 
     YTSearch({ key: API_KEY, term: 'cryptocurrencies'}, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
   render() {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={ this.state.videos }/>
+        <VideoDetail video={ this.state.selectedVideo } />
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+          videos={ this.state.videos } />
       </div>
     );
   }
